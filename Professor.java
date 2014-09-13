9c133da021dc2d3fc0bb1e74cf596fd981193ece
krithivasanchandran <c.krithivasan@gmail.com>
2014-09-13 00:50:34 -0500

    Professor BEAN CLASS

---
+++ Professor.java
@@ -0,0 +1,35 @@
+
+public class Professor {
+
+	private String OriginatedProfessorName;
+
+	private String departmentName;
+	
+	private String SchoolName;
+	
+	public String getOriginatedProfessorName() {
+		return OriginatedProfessorName;
+	}
+
+	public void setOriginatedProfessorName(String originatedProfessorName) {
+		OriginatedProfessorName = originatedProfessorName;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public String getSchoolName() {
+		return SchoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		SchoolName = schoolName;
+	}
+
+	
+}
